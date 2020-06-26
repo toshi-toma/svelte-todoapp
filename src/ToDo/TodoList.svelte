1c6c648a7c6d51dd2eb1b29698a60d727d871d04
toshi-toma <toshitoma1217@gmail.com>
2020-06-26 11:10:33 +0900

    doneボタンで完了

--- src/ToDo/TodoList.svelte
+++ src/ToDo/TodoList.svelte
@@ -12,8 +12,15 @@
     width: 300px;
   }
   .todo-item {
+    display: flex;
+    padding-bottom: 6px;
+    align-items: center;
     border-bottom: 1px solid #f0f0f0;
     margin-bottom: 16px;
+  }
+  .doneCheckbox {
+    margin-bottom: 0;
+    margin-right: 6px;
   }
 </style>
 
@@ -21,6 +28,11 @@
   <ul class="todo-list">
     {#each todos as todo}
       <li class="todo-item">
+        <input
+          type="checkbox"
+          class="doneCheckbox"
+          title="Done"
+          bind:checked={todo.isDone} />
         <div>{todo.content}</div>
       </li>
     {/each}
