50f26321649304653bffc65fd48054cec9a2e7f5
toshi-toma <toshitoma1217@gmail.com>
2020-06-26 11:54:46 +0900

    フィルターの実装

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,23 @@
 <script>
   import Header from "./Header.svelte";
   import AddTodoForm from "./ToDo/AddTodoForm.svelte";
+  import Filter from "./Filter.svelte";
   import TodoList from "./ToDo/TodoList.svelte";
 
   let todos = [];
+  let filter = "active"; // active | all
   const addTodo = todo => {
     todos = [...todos, { content: todo, isDone: false }];
   };
+  const updateFilter = nextFilter => (filter = nextFilter);
+  $: filteredTodos = todos.filter(todo => {
+    if (filter === "active") {
+      return !todo.isDone;
+    } else if (filter === "all") {
+      return true;
+    }
+    return true; // fallback
+  });
 </script>
 
 <style>
@@ -21,5 +32,6 @@
 <Header />
 <main>
   <AddTodoForm {addTodo} />
-  <TodoList {todos} />
+  <Filter bind:selectFilter={filter} {updateFilter} />
+  <TodoList bind:todos={filteredTodos} />
 </main>
---
+++ src/Filter.svelte
@@ -0,0 +1,19 @@
+<script>
+  export let selectFilter;
+  export let updateFilter;
+  const handleAllFilterSelect = () => updateFilter("all");
+  const handleActiveFilterSelect = () => updateFilter("active");
+</script>
+
+<style>
+
+</style>
+
+<ul>
+  <li>
+    <button on:click={handleAllFilterSelect}>All</button>
+  </li>
+  <li>
+    <button on:click={handleActiveFilterSelect}>Active</button>
+  </li>
+</ul>
