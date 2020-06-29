dbc9ebd2f3cef902fb85bd3319a6451e09e3c4bf
toshi-toma <toshitoma1217@gmail.com>
2020-06-29 15:33:11 +0900

    編集機能を実装

---
+++ src/ToDo/TodoItem.svelte
@@ -0,0 +1,46 @@
+<script>
+  export let todo;
+  export let deleteTodo;
+  let isEditing = false;
+  const toggleEditing = () => (isEditing = !isEditing);
+</script>
+
+<style>
+  .todo-item {
+    display: flex;
+    height: 30px;
+    padding-bottom: 6px;
+    align-items: center;
+    border-bottom: 1px solid #f0f0f0;
+    margin-bottom: 16px;
+  }
+  .doneCheckbox {
+    margin-bottom: 0;
+    margin-right: 6px;
+  }
+  .delete-button {
+    background-color: rgba(255, 255, 255, 0);
+    border: none;
+    margin-left: auto;
+    margin-bottom: 0;
+  }
+  .delete-button::after {
+    content: "x";
+  }
+</style>
+
+<li class="todo-item">
+  <input
+    type="checkbox"
+    class="doneCheckbox"
+    title="Done"
+    bind:checked={todo.isDone} />
+  {#if isEditing}
+    <form on:submit={toggleEditing}>
+      <input type="text" bind:value={todo.content} />
+    </form>
+  {:else}
+    <div on:click={toggleEditing}>{todo.content}</div>
+  {/if}
+  <button class="delete-button" on:click={() => deleteTodo(todo.id)} />
+</li>
--- src/ToDo/TodoList.svelte
+++ src/ToDo/TodoList.svelte
@@ -1,4 +1,5 @@
 <script>
+  import TodoItem from "./TodoItem.svelte";
   export let todos;
   export let deleteTodo;
 </script>
@@ -12,40 +13,12 @@
     list-style-type: none;
     width: 300px;
   }
-  .todo-item {
-    display: flex;
-    padding-bottom: 6px;
-    align-items: center;
-    border-bottom: 1px solid #f0f0f0;
-    margin-bottom: 16px;
-  }
-  .doneCheckbox {
-    margin-bottom: 0;
-    margin-right: 6px;
-  }
-  .delete-button {
-    background-color: rgba(255, 255, 255, 0);
-    border: none;
-    margin-left: auto;
-    margin-bottom: 0;
-  }
-  .delete-button::after {
-    content: "x";
-  }
 </style>
 
 <div class="wrapper">
   <ul class="todo-list">
     {#each todos as todo}
-      <li class="todo-item">
-        <input
-          type="checkbox"
-          class="doneCheckbox"
-          title="Done"
-          bind:checked={todo.isDone} />
-        <div>{todo.content}</div>
-        <button class="delete-button" on:click={() => deleteTodo(todo.id)} />
-      </li>
+      <TodoItem {todo} {deleteTodo} />
     {/each}
   </ul>
 </div>
